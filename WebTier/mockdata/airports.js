04d44ef2c6bc2a6efe0a22dc7eddc1f9f525f2b7
Cortes Hernandez <hercamil@8c8590b6c5ff.ant.amazon.com>
2019-10-18 18:52:51 -0500

    adding code

---
+++ WebTier/mockdata/airports.js
@@ -0,0 +1,37 @@
+airports = [
+    {
+      "IATA": "CUN",
+      "Airport": "Canc˙n International Airport LOCAL",
+      "Location": "Canc˙n, Quintana Roo, Mexico"
+    },
+    {
+      "IATA": "EZE",
+      "Airport": "Ministro Pistarini International Airport",
+      "Location": "Buenos Aires, Argentina"
+    },
+    {
+      "IATA": "LAS",
+      "Airport": "McCarran International Airport",
+      "Location": "Las Vegas, Nevada, United States"
+    },
+    {
+      "IATA": "MIA",
+      "Airport": "Miami International Airport",
+      "Location": "Miami, Florida, United States"
+    },
+    {
+      "IATA": "ORD",
+      "Airport": "O'Hare International Airport",
+      "Location": "Chicago, Illinois, United States"
+    },
+    {
+      "IATA": "PTY",
+      "Airport": "Tocumen International Airport",
+      "Location": "Panama City, Panama"
+    },
+    {
+      "IATA": "YYZ",
+      "Airport": "Toronto Pearson International Airport",
+      "Location": "Toronto, Ontario, Canada"
+    }
+  ]
---
+++ WebTier/mockdata/distance.js
@@ -0,0 +1,262 @@
+distance = {
+  "distanceList": [
+    {
+      "ORIG_CD": "CUN",
+      "DEST_CD": "EZE",
+      "MILES": 4282,
+      "O&D": "CUN-EZE"
+    },
+    {
+      "ORIG_CD": "CUN",
+      "DEST_CD": "LAS",
+      "MILES": 1996,
+      "O&D": "CUN-LAS"
+    },
+    {
+      "ORIG_CD": "CUN",
+      "DEST_CD": "MIA",
+      "MILES": 532,
+      "O&D": "CUN-MIA"
+    },
+    {
+      "ORIG_CD": "CUN",
+      "DEST_CD": "ORD",
+      "MILES": 1444,
+      "O&D": "CUN-ORD"
+    },
+    {
+      "ORIG_CD": "CUN",
+      "DEST_CD": "PTY",
+      "MILES": 963,
+      "O&D": "CUN-PTY"
+    },
+    {
+      "ORIG_CD": "CUN",
+      "DEST_CD": "YYZ",
+      "MILES": 1614,
+      "O&D": "CUN-YYZ"
+    },
+
+    {
+      "ORIG_CD": "EZE",
+      "DEST_CD": "CUN",
+      "MILES": 4282,
+      "O&D": "EZE-CUN"
+    },
+    {
+      "ORIG_CD": "EZE",
+      "DEST_CD": "LAS",
+      "MILES": 6091,
+      "O&D": "EZE-LAS"
+    },
+    {
+      "ORIG_CD": "EZE",
+      "DEST_CD": "MIA",
+      "MILES": 4421,
+      "O&D": "EZE-MIA"
+    },
+    {
+      "ORIG_CD": "EZE",
+      "DEST_CD": "ORD",
+      "MILES": 5603,
+      "O&D": "EZE-ORD"
+    },
+    {
+      "ORIG_CD": "EZE",
+      "DEST_CD": "PTY",
+      "MILES": 3313,
+      "O&D": "EZE-PTY"
+    },
+    {
+      "ORIG_CD": "EZE",
+      "DEST_CD": "YYZ",
+      "MILES": 5572,
+      "O&D": "EZE-YYZ"
+    },
+
+    {
+      "ORIG_CD": "LAS",
+      "DEST_CD": "CUN",
+      "MILES": 1996,
+      "O&D": "LAS-CUN"
+    },
+    {
+      "ORIG_CD": "LAS",
+      "DEST_CD": "EZE",
+      "MILES": 6091,
+      "O&D": "LAS-EZE"
+    },
+    {
+      "ORIG_CD": "LAS",
+      "DEST_CD": "MIA",
+      "MILES": 2170,
+      "O&D": "LAS-MIA"
+    },
+    {
+      "ORIG_CD": "LAS",
+      "DEST_CD": "ORD",
+      "MILES": 1514,
+      "O&D": "LAS-ORD"
+    },
+    {
+      "ORIG_CD": "LAS",
+      "DEST_CD": "PTY",
+      "MILES": 2917,
+      "O&D": "LAS-PTY"
+    },
+    {
+      "ORIG_CD": "LAS",
+      "DEST_CD": "YYZ",
+      "MILES": 1939,
+      "O&D": "LAS-YYZ"
+    },
+
+    {
+      "ORIG_CD": "MIA",
+      "DEST_CD": "CUN",
+      "MILES": 532,
+      "O&D": "MIA-CUN"
+    },
+    {
+      "ORIG_CD": "MIA",
+      "DEST_CD": "EZE",
+      "MILES": 4421,
+      "O&D": "MIA-EZE"
+    },
+    {
+      "ORIG_CD": "MIA",
+      "DEST_CD": "LAS",
+      "MILES": 2170,
+      "O&D": "MIA-LAS"
+    },
+    {
+      "ORIG_CD": "MIA",
+      "DEST_CD": "ORD",
+      "MILES": 1197,
+      "O&D": "MIA-ORD"
+    },
+    {
+      "ORIG_CD": "MIA",
+      "DEST_CD": "PTY",
+      "MILES": 1152,
+      "O&D": "MIA-PTY"
+    },
+    {
+      "ORIG_CD": "MIA",
+      "DEST_CD": "YYZ",
+      "MILES": 1236,
+      "O&D": "MIA-YYZ"
+    },
+
+    {
+      "ORIG_CD": "ORD",
+      "DEST_CD": "CUN",
+      "MILES": 1444,
+      "O&D": "ORD-CUN"
+    },
+    {
+      "ORIG_CD": "ORD",
+      "DEST_CD": "EZE",
+      "MILES": 5603,
+      "O&D": "ORD-EZE"
+    },
+    {
+      "ORIG_CD": "ORD",
+      "DEST_CD": "LAS",
+      "MILES": 1514,
+      "O&D": "ORD-LAS"
+    },
+    {
+      "ORIG_CD": "ORD",
+      "DEST_CD": "MIA",
+      "MILES": 1197,
+      "O&D": "ORD-MIA"
+    },
+    {
+      "ORIG_CD": "ORD",
+      "DEST_CD": "PTY",
+      "MILES": 2325,
+      "O&D": "ORD-PTY"
+    },
+    {
+      "ORIG_CD": "ORD",
+      "DEST_CD": "YYZ",
+      "MILES": 437,
+      "O&D": "ORD-YYZ"
+    },
+
+    {
+      "ORIG_CD": "PTY",
+      "DEST_CD": "CUN",
+      "MILES": 963,
+      "O&D": "PTY-CUN"
+    },
+    {
+      "ORIG_CD": "PTY",
+      "DEST_CD": "EZE",
+      "MILES": 3313,
+      "O&D": "PTY-EZE"
+    },
+    {
+      "ORIG_CD": "PTY",
+      "DEST_CD": "LAS",
+      "MILES": 2917,
+      "O&D": "PTY-LAS"
+    },
+    {
+      "ORIG_CD": "PTY",
+      "DEST_CD": "MIA",
+      "MILES": 1152,
+      "O&D": "PTY-MIA"
+    },
+    {
+      "ORIG_CD": "PTY",
+      "DEST_CD": "ORD",
+      "MILES": 2325,
+      "O&D": "PTY-ORD"
+    },
+    {
+      "ORIG_CD": "PTY",
+      "DEST_CD": "YYZ",
+      "MILES": 2383,
+      "O&D": "PTY-YYZ"
+    },
+
+    {
+      "ORIG_CD": "YYZ",
+      "DEST_CD": "CUN",
+      "MILES": 1614,
+      "O&D": "YYZ-CUN"
+    },
+    {
+      "ORIG_CD": "YYZ",
+      "DEST_CD": "EZE",
+      "MILES": 5572,
+      "O&D": "YYZ-EZE"
+    },
+    {
+      "ORIG_CD": "YYZ",
+      "DEST_CD": "LAS",
+      "MILES": 1939,
+      "O&D": "YYZ-LAS"
+    },
+    {
+      "ORIG_CD": "YYZ",
+      "DEST_CD": "MIA",
+      "MILES": 1236,
+      "O&D": "YYZ-MIA"
+    },
+    {
+      "ORIG_CD": "YYZ",
+      "DEST_CD": "ORD",
+      "MILES": 437,
+      "O&D": "YYZ-ORD"
+    },
+    {
+      "ORIG_CD": "YYZ",
+      "DEST_CD": "PTY",
+      "MILES": 2383,
+      "O&D": "YYZ-PTY"
+    }
+  ]
+}
