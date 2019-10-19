251b0f5a66e08755c002bcec751dc9a8956d1830
EC2 Default User <ec2-user@ip-172-31-90-164.ec2.internal>
2019-10-19 20:13:12 +0000

    adding cors

--- AppTier/index.js
+++ AppTier/index.js
@@ -1,10 +1,25 @@
 const express = require('express'),
   app = express(),
   bodyParser = require('body-parser');
-var cors = require('cors');
 var port = 3000;
 
+var cors = require('cors');
 app.use(cors());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+
+    // authorized headers for preflight requests
+    // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+
+    app.options('*', (req, res) => {
+        // allowed XHR methods  
+        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+        res.send();
+    });
+});
 
 const mysql = require('mysql');
 // connection configurations
